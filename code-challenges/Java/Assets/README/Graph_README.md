efe9e107e13223ba9ecdebbd99de1e310ffdf77f
Chai.Narukulla <chaitanyanarukulla@gmail.com>
2019-07-17 11:03:18 -0700

    Compleated bft graph with passing test

---
+++ code-challenges/Java/Assets/README/Graph_README.md
@@ -0,0 +1,23 @@
+# Graph Implementation
+- Implement a graph. The graph should be represented as an adjacency list
+
+## Challenge
+- Should be able to add a node to the graph. Adds a new node to the graph, Takes in the value of that node, Returns the added node.
+- Add an edge to the graph. Adds a new edge between two nodes in the graph, Include the ability to have a “weight”, Takes in the two nodes to be connected by the edge, Both nodes should already be in the Graph.
+- Get all of the nodes in a graph. Returns all of the nodes in the graph as a collection (set, list, or similar).
+- Get all the neighbors of a node. Returns a collection of nodes connected to the given node, takes in a given node, Include the weight of the connection in the returned collection.
+- Get the size of a graph. Returns the total number of nodes in the graph.                                 
+- A breadth-first traversal method that accepts a starting node. Return a collection of nodes in the order they were visited. 
+
+## Approach & Efficiency
+- breadthFirstTraversal Graph has a time O(n) and space O(n)
+
+## API and Acknowledgements
+- addNeighbor(Node<T> node, int weight)
+- depthFirstTraversal(Node startingNode)
+- addNode(T data)
+- addEdge(Node<T> source, Node<T> destination)
+- Michelle's in class example for traversing a graph (in her example it was for size)
+
+## Whiteboard Picture
+![](../images/Graph.jpg)
--- code-challenges/Java/src/main/java/graph/Graph.java
+++ code-challenges/Java/src/main/java/graph/Graph.java
@@ -1,4 +1,79 @@
 package graph;
 
-public class Graph {
+import java.lang.reflect.Array;
+import java.util.*;
+
+public class Graph<T> {
+
+    public HashSet<Node<T>> nodes;
+
+    public Graph(){
+        this.nodes = new HashSet<>();
+    }
+
+    public Node<T>addNode(T data){
+        Node node = new Node(data);
+        this.nodes.add(node);
+        return node;
+    }
+
+    public boolean addEdge(Node<T> source, Node<T> destination){
+        if(this.nodes.contains(source) && this.nodes.contains(destination)){
+            this.nodes.add(source);
+            this.nodes.add(destination);
+            return true;
+        }
+        return false;
+    }
+
+    public HashSet<Node<T>>getNodes(){
+        return this.nodes;
+    }
+
+    public HashSet<Edge> getNeighbors(Node<T> node){
+        return node.neighbors;
+    }
+
+    public int getNodeNum(){
+        return this.nodes.size();
+    }
+
+    public static ArrayList<Node> breadthFirstTraversal(Node startingNode){
+        HashSet<Node> visited = new HashSet<>();
+        ArrayList answer = new ArrayList();
+        Queue tempQueue  = new LinkedList();
+        tempQueue.add(startingNode);
+        visited.add(startingNode);
+        while(!tempQueue.isEmpty()){
+            Node tempNode = (Node) tempQueue.remove();
+            answer.add(tempNode);
+            for(Edge neighbor: (HashSet<Edge>) tempNode.neighbors){
+                if(!visited.contains(neighbor.node)){
+                    tempQueue.add(neighbor.node);
+                    visited.add(neighbor.node);
+                }
+            }
+        }
+        return answer;
+    }
+
+    public static ArrayList<Node> depthFirstTraversal(Node startingNode){
+        HashSet<Node> visited = new HashSet<>();
+        ArrayList answer = new ArrayList();
+        Stack tempStack  = new Stack();
+        tempStack.push(startingNode);
+        visited.add(startingNode);
+        while(!tempStack.isEmpty()){
+            Node tempNode = (Node) tempStack.pop();
+            answer.add(tempNode);
+            for(Edge neighbor: (HashSet<Edge>) tempNode.neighbors){
+                if(!visited.contains(neighbor.node)){
+                    tempStack.push(neighbor.node);
+                    visited.add(neighbor.node);
+                }
+            }
+        }
+        return answer;
+    }
+
 }
